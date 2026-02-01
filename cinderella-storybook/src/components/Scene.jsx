72180735a62cdb1a5d10150b6ec1f49dfba9d984
Birana19 <mahinbiranu@gmail.com>
2026-02-01 22:39:45 +0530

    Scenes update

--- cinderella-storybook/src/components/Scene.jsx
+++ cinderella-storybook/src/components/Scene.jsx
@@ -1,16 +1,14 @@
 // src/components/Scene.jsx
+import "../styles/scene.css";
 import "../styles/animations.css";
 
 function Scene({ scene }) {
   return (
-    <div className="scene fade-in">
-      <img
-        src={scene.image}
-        alt={scene.title}
-        className="scene-image"
-      />
-
-      <div className="scene-content">
+    <div
+      className="scene fade-in"
+      style={{ backgroundImage: `url(${scene.image})` }}
+    >
+      <div className="scene-overlay">
         <h2>{scene.title}</h2>
         <p>{scene.text}</p>
       </div>
